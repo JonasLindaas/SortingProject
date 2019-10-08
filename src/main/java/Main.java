2770e7f50cfff89714c45257f9de0f100f37164c
Jonas Drageset Lindaas <Jonas.Lindaas@student.uib.no>
2019-10-08 14:02:23 +0200

    Implemented ISortingProject with a class, needs testing
    Needs more comments and explanations as well
    
    Main has also been modified, but needs to be tested (didn't have the time yet)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import IO.ConsoleOutputHandler;
 import IO.IInputHandler;
 import IO.IOutputHandler;
 import Processors.CollectionsSort;
+import Processors.IParser;
 import Processors.ISort;
 import Processors.Parser;
 import Storage.IItem;
@@ -11,7 +12,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+    // Change these if you want to use another class instead
+    private static final IInputHandler INPUT_HANDLER   = new ConsoleInputHandler();
+    private static final IParser PARSER                = new Parser();
+    private static final ISort SORTER                  = new CollectionsSort();
+    private static final IOutputHandler OUTPUT_HANDLER = new ConsoleOutputHandler();
+
+
+
     public static void main(String[] args) {
+        ISortingProject sortingProject = new SortingProject(INPUT_HANDLER, PARSER, SORTER, OUTPUT_HANDLER);
+        sortingProject.performDefaultBehaviour();
+
+        /* Old code TODO: remove if default behaviour works
         Parser parser = new Parser();
         IInputHandler inputHandler = new ConsoleInputHandler();
         ArrayList<String> input = inputHandler.readInput();
@@ -31,6 +44,6 @@ public class Main {
             itemNames.add(item.getName());
         }
         outputHandler.outputWithSMFormatting(itemNames);
-
+        */
     }
 }
---
+++ src/main/java/SortingProject.java
@@ -0,0 +1,137 @@
+import IO.IInputHandler;
+import IO.IOutputHandler;
+import Processors.IParser;
+import Processors.ISort;
+import Storage.IItem;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+//TODO: needs comments
+public class SortingProject implements ISortingProject {
+    private ArrayList<String> input         = new ArrayList<>();
+    private ArrayList<IItem> originalOrder  = new ArrayList<>();
+    private ArrayList<IItem> sortedOrder    = new ArrayList<>();
+
+    //TODO: should consider making a constructor that creates "default" objects for the various processors
+    private IInputHandler inputHandler;
+    private IParser parser;
+    private ISort sorter;
+    private IOutputHandler outputHandler;
+
+    public SortingProject(IInputHandler inputHandler, IParser parser, ISort sorter, IOutputHandler outputHandler) {
+        this.inputHandler = inputHandler;
+        this.parser = parser;
+        this.sorter = sorter;
+        this.outputHandler = outputHandler;
+    }
+
+    @Override
+    public void readInput() {
+        if(input.isEmpty()) {
+            input = inputHandler.readInput();
+        } else {
+            throwIllegalAccessError("Input has already been read");
+        }
+    }
+
+    private void throwIllegalAccessError(String msg) {
+        throwIllegalAccessError(msg); //TODO: consider making exception
+    }
+
+    @Override
+    public void parseInput() {
+        if(input.isEmpty()) {
+            System.out.println("Input was empty, attempting to read input:");
+            //TODO: make this a try catch block (it's been a while since I wrote one ok?)
+            readInput();
+            System.out.println("Success! Continuing parsing");
+        }
+
+        if(originalOrder.isEmpty()) {
+            for (String s : input)
+                originalOrder.add(parser.createItem(s));
+        } else {
+            throwIllegalAccessError("Items have already been created, and can not be changed after this point");
+        }
+    }
+
+    @Override
+    public void printOutput(boolean sortedItems) {
+        List<IItem> items = makeSureListIsNotEmpty(sortedItems);
+        List<String> itemNames = new ArrayList<>();
+        for(IItem item : items) {
+            itemNames.add(item.getName());
+        }
+        outputHandler.outputWithSMFormatting(itemNames);
+    }
+
+    @Override
+    public void printItems(boolean sortedItems) {
+        List<IItem> items = makeSureListIsNotEmpty(sortedItems);
+        for(IItem item : items) {
+            item.printToConsole();
+        }
+    }
+
+    private List<IItem> makeSureListIsNotEmpty(boolean sortedItems) {
+        List<IItem> items;
+        if(sortedItems) {
+            items = sortedOrder;
+        } else {
+            items = originalOrder;
+        }
+
+        if(items.isEmpty()) { //Technically sorting might be redundant, but it doesn't hurt, and makes the logic simpler
+            System.out.println("The given list was empty, attempting to sort:");
+            //TODO: make this a try catch block (it's been a while since I wrote one ok?)
+            sort();
+            System.out.println("Success! Continuing to print output");
+        }
+        return items;
+    }
+
+    @Override
+    public void setSorter(ISort sorter) {
+        this.sorter = sorter;
+    }
+
+    @Override
+    public void sort() {
+        if(originalOrder.isEmpty()) {
+            System.out.println("originalOrder was empty, attempting to parse:");
+            //TODO: make this a try catch block (it's been a while since I wrote one ok?)
+            parseInput();
+            System.out.println("Success! Continuing to sort");
+        }
+
+        ArrayList<IItem> listToSort;
+        if(!sortedOrder.isEmpty()) {
+            listToSort = sortedOrder;
+        } else {
+            listToSort = (ArrayList<IItem>) originalOrder.clone();
+        }
+
+        sorter.sort(listToSort);
+        sortedOrder = listToSort;
+    }
+
+    @Override
+    public List<IItem> getItemsInOriginalOrder() { //TODO: consider making this and the next method iterators instead
+        return originalOrder;
+    }
+
+    @Override
+    public List<IItem> getItemsInSortedOrder() {
+        return sortedOrder;
+    }
+
+    @Override
+    public void performDefaultBehaviour() {
+        readInput();    //TODO: can probably shorten this to simply call sort() (because all of the methods call each other if needed
+        parseInput();
+        sort();
+        printOutput(true);
+    }
+}
