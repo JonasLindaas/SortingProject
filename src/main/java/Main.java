3d4c92b32976b45cdcc5c5863d7fe2f9409d2a23
JonasLindaas <jonas.drageset.lindaas@gmail.com>
2019-10-22 20:33:30 +0200

    Cleaned up Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,10 +6,6 @@ import Processors.CollectionsSort;
 import Processors.IParser;
 import Processors.ISort;
 import Processors.Parser;
-import Storage.IItem;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     // Change these if you want to use another class instead
@@ -23,27 +19,5 @@ public class Main {
     public static void main(String[] args) {
         ISortingProject sortingProject = new SortingProject(INPUT_HANDLER, PARSER, SORTER, OUTPUT_HANDLER);
         sortingProject.performDefaultBehaviour();
-
-        /* Old code TODO: remove if default behaviour works
-        Parser parser = new Parser();
-        IInputHandler inputHandler = new ConsoleInputHandler();
-        ArrayList<String> input = inputHandler.readInput();
-
-        List<IItem> items = new ArrayList<>();
-        for(String s : input) {
-            items.add(parser.createItem(s));
-        }
-
-        ISort sorter = new CollectionsSort();
-        sorter.sort(items);
-
-        IOutputHandler outputHandler = new ConsoleOutputHandler();
-
-        ArrayList<String> itemNames = new ArrayList<>();
-        for(IItem item : items) {
-            itemNames.add(item.getName());
-        }
-        outputHandler.outputWithSMFormatting(itemNames);
-        */
     }
 }
